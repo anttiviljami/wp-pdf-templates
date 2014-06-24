7b331e32bdb3a1fb1748ea7f74207cf7fd3556b9
Antti Kuosmanen <antti@seravo.fi>
2014-06-24 09:51:42 +0300

    Revert & enable load_font.php CLI tool
    We received special permission from WP!

--- load_font.php
+++ load_font.php
@@ -11,20 +11,10 @@
 
 /*
  * Require the WP Environment
- *
- * Wordpress.org actually doesn't allow us to do this from outside of WP
- * itself. To make this plugin pass through the wp.org plugin validation
- * process, we've had to disable this script for now.
- *
- * Instructions below on how to re-enable at your own discretion:
  */
 
 // This is where we require wp-load. Just uncomment the line below.
-// require_once base64_decode("Li4vLi4vLi4vd3AtbG9hZC5waHA=");
-
-// Don't forget to comment this! ->
-exit("Sorry! Wordpress.org didn't allow us to include this command line tool in the plugin. If you wish to use it (at your own discretion), you can to edit this file to require wp-load. \n");
-// <- Don't forget to comment this!
+require_once "../../../wp-load.php";
 
 /*
  * Pass to the DOMPDF load_font command line tool
