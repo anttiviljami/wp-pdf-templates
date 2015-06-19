39005e19ead247ec2fac3219e076494b35bd26d7
Onni Hakala <onni@koodimonni.fi>
2015-06-19 14:56:10 +0300

    had wrong plugin name here

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ $GLOBALS['wp_tests_options'] = array(
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __DIR__ ) . '/wp-dashboard-log-monitor.php';
+	require dirname( __DIR__ ) . '/'.PLUGIN_NAME;
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
